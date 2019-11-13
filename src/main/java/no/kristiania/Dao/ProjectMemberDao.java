e79e91ce5a19ecaaf8ba2afab1e43c3a2eb256ec
LASSE <lassehovden@gmail.com>
2019-11-13 11:09:11 +0100

    added seprate html sheets for select worker and add worker

--- src/main/java/no/kristiania/Dao/ProjectMemberDao.java
+++ src/main/java/no/kristiania/Dao/ProjectMemberDao.java
@@ -1,19 +1,11 @@
 package no.kristiania.Dao;
 
-import org.flywaydb.core.Flyway;
-import org.postgresql.ds.PGSimpleDataSource;
-
 import javax.sql.DataSource;
-import java.io.FileReader;
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Properties;
-import java.util.Scanner;
 
 public class ProjectMemberDao extends AbstractDao<ProjectMember> {
 
