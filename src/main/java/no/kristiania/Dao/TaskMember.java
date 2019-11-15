2b7776736f16c70804c7d19ae9bf075c7f91cbfa
LASSE <lassehovden@gmail.com>
2019-11-15 20:16:06 +0100

    added dao moethods to taskmember

---
+++ src/main/java/no/kristiania/Dao/TaskMember.java
@@ -0,0 +1,54 @@
+package no.kristiania.Dao;
+
+import java.util.Objects;
+
+public class TaskMember {
+    private long memberId;
+    private long taskId= 1;
+
+
+    public TaskMember() {
+        this.memberId = memberId;
+        this.taskId = taskId;
+    }
+
+
+
+    public long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(long memberId) {
+        this.memberId = memberId;
+    }
+
+    public long getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(long taskId) {
+        this.taskId = taskId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TaskMember that = (TaskMember) o;
+        return memberId == that.memberId &&
+                taskId == that.taskId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(memberId, taskId);
+    }
+
+    @Override
+    public String toString() {
+        return "TaskMember{" +
+                "memberId=" + memberId +
+                ", taskId=" + taskId +
+                '}';
+    }
+}
--- src/main/java/no/kristiania/Dao/TaskMemberDao.java
+++ src/main/java/no/kristiania/Dao/TaskMemberDao.java
@@ -1,4 +1,72 @@
 package no.kristiania.Dao;
 
-public class TaskMemberDao extends AbstractDao<Status>{{
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+public class TaskMemberDao extends AbstractDao<TaskMember>{{
+
+
+}
+
+    public TaskMemberDao(DataSource dataSource) {
+        super(dataSource);
+    }
+
+    @Override
+    protected void insertMember(TaskMember taskMember, PreparedStatement statement) throws SQLException {
+        statement.setLong(1, taskMember.getMemberId());
+        statement.setLong(2, taskMember.getTaskId());
+    }
+
+    @Override
+    protected TaskMember readObject(ResultSet resultSet) throws SQLException {
+        TaskMember taskMember = new TaskMember();
+        taskMember.setTaskId(resultSet.getLong(2));
+        taskMember.setMemberId(resultSet.getLong(1));
+        return taskMember;
+    }
+
+    public List<TaskMember> listAll() throws SQLException {
+        return listAll("select * from taskmembers");
+    }
+
+    public long insert(TaskMember taskMember) throws SQLException {
+        long id = insert(taskMember, "insert into taskmembers (member_id,task_id) values (?,?)");
+        taskMember.setMemberId((int) id);
+        taskMember.setTaskId((int) id);
+        return id;
+    }
+
+    public TaskMember retrieveTaskID(long id) throws SQLException{
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement("select * from status where id = ?")) {
+                statement.setLong(1, id);
+                try (ResultSet resultSet = statement.executeQuery()) {
+                    if(resultSet.next()) {
+                        return (readObject(resultSet));
+                    } else {
+                        return null;
+                    }
+                }
+            }
+        }
+        public TaskMember retrieveMemberId(long id) throws SQLException{
+            try (Connection connection = dataSource.getConnection()) {
+                try (PreparedStatement statement = connection.prepareStatement("select * from status where id = ?")) {
+                    statement.setLong(1, id);
+                    try (ResultSet resultSet = statement.executeQuery()) {
+                        if(resultSet.next()) {
+                            return (readObject(resultSet));
+                        } else {
+                            return null;
+                        }
+                    }
+                }
+            }
+    }
+
 }
