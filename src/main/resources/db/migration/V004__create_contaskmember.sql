81cbfb588b110ebbd9f4d997b537719067d5e442
LASSE <lassehovden@gmail.com>
2019-11-15 18:24:12 +0100

    added new connection table

--- src/main/resources/db/migration/V004__create_contaskmember.sql
+++ src/main/resources/db/migration/V004__create_contaskmember.sql
@@ -3,8 +3,8 @@
 
 create table taskmembers
 (
-    member_id int,
-    task_id int
+    member_id int  REFERENCES projectmembers (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    task_id int  REFERENCES tasks (id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 
